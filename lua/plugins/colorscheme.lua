81234c7c2ec24e7b1320f59a707af2fff193d6ea
michaelhaaf <michael.haaf@gmail.com>
2024-11-17 17:10:00 -0500

    add colorscheme and quarto plugins and customizations

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "moon",
+      styles = {
+        transparency = true,
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
+    "tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
