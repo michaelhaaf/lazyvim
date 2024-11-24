3b2933c6d57e12d46d324b61ad9a32f3f701afc2
michaelhaaf <michael.haaf@gmail.com>
2024-11-24 15:41:47 -0500

    remove janky dashboard features

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -22,20 +22,6 @@ return {
 ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
         },
         sections = {
-          {
-            pane = 2,
-            section = "terminal",
-            cmd = "chafa ~/media/pictures/wallpapers/nvim.jpg --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
-            height = 17,
-            padding = 1,
-          },
-          {
-            pane = 1,
-            section = "terminal",
-            cmd = "colorscript -e square",
-            height = 5,
-            padding = 1,
-          },
           { pane = 1, section = "header" },
           { pane = 1, section = "keys", gap = 1, padding = 1 },
           { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
