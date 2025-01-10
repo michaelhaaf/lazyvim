d3dd64ad531d5ba639aaea88bbcde55643cbca98
michaelhaaf <michael.haaf@gmail.com>
2025-01-10 13:17:17 -0500

    add window picker

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -134,4 +134,15 @@ return {
     },
     cmd = "Oil",
   },
+  {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup({
+        hint = "floating-big-letter",
+      })
+    end,
+  },
 }
