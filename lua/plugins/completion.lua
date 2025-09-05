65a27bb261a5ca9b1313a27619a941cd4031bcbe
michaelhaaf <michael.haaf@gmail.com>
2025-09-05 13:26:41 -0400

    remove annoying autoappearing ui

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,11 +1,43 @@
 return {
   {
     "saghen/blink.cmp",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "moyiz/blink-emoji.nvim",
+      "Kaiser-Yang/blink-cmp-dictionary",
+    },
     opts = {
       keymap = {
         preset = 'default',
       },
+      snippets = {
+        preset = "luasnip",
+      },
+      completion = {
+        documentation = {
+          auto_show = false,
+        },
+        ghost_text = {
+          enabled = false,
+        },
+      },
+      sources = {
+        compat = {},
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
     },
   },
+
+  {
+    "folke/noice.nvim",
+    optional = true,
+    opts = {
+      lsp = {
+        signature = {
+          auto_open = { enabled = false },
+        },
+      },
+    },
+  }
 
 }
