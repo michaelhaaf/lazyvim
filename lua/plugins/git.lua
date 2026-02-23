040f8ea8993be966fe2666dc886e8e005969bc0f
michaelhaaf <michael.haaf@gmail.com>
2026-02-23 01:21:02 -0500

    some plugins

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,28 @@
 -- git plugins
 
 return {
+  { "rhysd/git-messenger.vim" },
+  { "tpope/vim-fugitive" },
+  { "sindrets/diffview.nvim" },
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
 
+      -- Only one of these is needed.
+      "sindrets/diffview.nvim", -- optional
+      -- "esmuellert/codediff.nvim", -- optional
+
+      -- Only one of these is needed.
+      -- "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua", -- optional
+      -- "nvim-mini/mini.pick", -- optional
+      "folke/snacks.nvim", -- optional
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gn", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+    },
+  },
 }
